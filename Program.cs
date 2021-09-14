01c2f1a79ed171dc344bd6fd516412ff29628a9c
TechnoCookiethePerson <66851745+TechnoCookiethePerson@users.noreply.github.com>
2021-09-14 10:45:24 -0300

    files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey");
+            Console.WriteLine("Press a key to see something cool");;
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Boom! Text is gone!");
+            Console.WriteLine("Pretty cool huh?");
+            Console.ReadKey();
+        }
+    }
+}
